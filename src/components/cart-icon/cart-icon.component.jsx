38700e056259650a68853250895c23e70711ff8b
pratik chauhan <pratik.chauhan2704@gmail.com>
2021-01-05 18:07:53 +0530

    add reselect package for cart-item and cart-dropdown

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,20 +1,25 @@
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { connect } from 'react-redux';
 
-import { toogleCartHidder } from '../../redux/cart/cart.action'
+import { toogleCartHidder } from '../../redux/cart/cart.action';
+import { selectItemCount } from '../../redux/cart/cart.selector';
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toogleCartHidder }) => {
+const CartIcon = ({ toogleCartHidder, itemCount }) => {
    return (
       <div className="cart-icon" onClick={() => toogleCartHidder()}> 
          <ShoppingIcon className="shopping-icon" />
-         <span className="item-count">0</span> 
-      </div> 
+         <span className="item-count">{itemCount}</span> 
+      </div>  
    ); 
 }
 
 const mapDispatchToProps = (dispatch) => {
    return { toogleCartHidder: () => dispatch(toogleCartHidder()) }
 }
+
+const mapStateToProps = (state) => {
+   return { itemCount: selectItemCount(state) };    
+}
  
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon); 
